39f68cb5c20900289af35d40ddd42a88b07e8df2
starry9x <rylily.huang@gmail.comgit config --global user.email>
2026-01-12 14:08:58 +0800

    Initial Flask project setup

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("homepage.html", title="Home")
+@app.route("/groups")
+def groups():
+    return render_template("groups.html", title="Groups")
+@app.route("/messages")
+def messages():
+    return render_template("messages.html", title="Messages")
+@app.route("/activities")
+def activities():
+    return render_template("activities.html", title="Activities")
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", title="Profile")
+
+if __name__ == '__main__':
+    app.run()
