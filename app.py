1c972f6836e4ac2f6d35969707d110c8ff4cddd1
Alyssa <252914T@mymail.nyp.edu.sg>
2026-01-13 17:55:47 +0800

    new folder, models, created. activites py moved

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'  # or MySQL/Postgres
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sharejoy.db'  # or MySQL/Postgres
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
